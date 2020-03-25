b0d6fc37f27f2c207aa3a671497724a19de4e921
agnynureza <agny.nureza@amarbank.co.id>
2020-03-25 23:26:12 +0700

    fibonanci solution 3

--- fibonancci/index.js
+++ fibonancci/index.js
@@ -9,14 +9,33 @@
 // 	}
 // }
 
-//solution 2 
-function getNthFib(n,init={1:0,2:1}){
-	if(n in init){
-		return init[n]
-	}else{
-		init[n] = getNthFib(n-1, init) + getNthFib(n-2,init)
-		return init[n]
+// //solution 2 
+// function getNthFib(n,init={1:0,2:1}){
+// 	if(n in init){
+// 		return init[n]
+// 	}else{
+// 		init[n] = getNthFib(n-1, init) + getNthFib(n-2,init)
+// 		return init[n]
+// 	}
+// }
+
+//solution 3
+function getNthFib(n){
+	let fibonacci = [0,1]
+	let counter = 3
+
+	if(n == 1){
+		return fibonacci[0]
 	}
+	while(counter <= n){
+		let nextFibonacci = fibonacci[0] + fibonacci[1]
+		fibonacci [0] = fibonacci[1]
+		fibonacci [1] = nextFibonacci
+		counter++
+	}
+
+	return n > 1 ? fibonacci[1] : fibonacci[0]
 }
 
 exports.getNthFib = getNthFib;
+
